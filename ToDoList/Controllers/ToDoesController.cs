f6f1483fe8cc76b07d6a6acf1873c564a95b20a4
Xiaoxiao1234567890 <rosaliu.567@gmail.com>
2017-07-24 18:13:28 +0100

    create items without refresh pages

--- ToDoList/Controllers/ToDoesController.cs
+++ ToDoList/Controllers/ToDoesController.cs
@@ -18,10 +18,21 @@ namespace ToDoList.Controllers
         // GET: ToDoes
         public ActionResult Index()
         {
+            return View();
+        }
+
+        private IEnumerable<ToDo> GetMyToDoes()
+        {
             string currentUserId = User.Identity.GetUserId();
             ApplicationUser currentUser = db.Users.FirstOrDefault
                 (x => x.Id == currentUserId);
-            return View(db.ToDos.ToList().Where(x=>x.User==currentUser));
+            return db.ToDos.ToList().Where(x => x.User == currentUser);
+        }
+
+        public ActionResult BuildToDoTable()
+        {
+           
+            return PartialView("_ToDoTable", GetMyToDoes());
         }
 
         // GET: ToDoes/Details/5
@@ -64,6 +75,24 @@ namespace ToDoList.Controllers
             } 
 
             return View(toDo);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AJAXCreate([Bind(Include = "Id,Description")] ToDo toDo)
+        {
+            if (ModelState.IsValid)
+            {
+                string currentUserId = User.Identity.GetUserId();
+                ApplicationUser currentUser = db.Users.FirstOrDefault
+                    (x => x.Id == currentUserId);
+                toDo.User = currentUser;
+                toDo.IsDone = false;
+                db.ToDos.Add(toDo);
+                db.SaveChanges();
+            }
+
+            return PartialView("_ToDoTable", GetMyToDoes());
         }
 
         // GET: ToDoes/Edit/5
